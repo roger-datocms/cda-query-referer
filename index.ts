29804c25deca18cc29c179e781b8fe34c6119695
Roger Tuan (DatoCMS) <r.tuan@datocms.com>
2026-04-21 12:30:28 -0700

    More thorough testing

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import {rawExecuteQuery} from "@datocms/cda-client";
+import {rawExecuteQuery, executeQuery, type ExecuteQueryOptions} from "@datocms/cda-client";
 import {randomUUID} from "node:crypto";
 
 const query = `
@@ -12,14 +12,25 @@ const query = `
 (async () => {
     const uuid = randomUUID();
     const randomInt = Math.floor(Math.random() * 100);
+
     const referer = `https://www.example${randomInt}.com/${uuid}/test?uuid=${uuid}&staticParam=test`; // Query strings are seemingly stripped by us?
-    await rawExecuteQuery(query, {
+
+    const queryOptions:ExecuteQueryOptions = {
         token: process.env.DATOCMS_API_TOKEN, // From https://cda-query-referer.admin.datocms.com/project_settings/access_tokens/398768/edit
         referer: referer, // This works
+        requestInitOptions: {
+            referrer: 'https://www.fake.com' // This does nothing, apparently...
+        },
         fetchFn: (url: RequestInfo | URL, init: RequestInit) => {
             console.log(`init.referrer: ${init.referrer}`); // This seems like a useless param? Maybe a mis-typing?
             console.log("Actual HTTP headers:", {...init.headers, Authorization: 'redacted'});
             return fetch(url, init);
         },
-    });
+    }
+
+    const queryResult = await executeQuery(query, queryOptions);
+    const rawQueryResult = await rawExecuteQuery(query,queryOptions);
+
+    console.log('queryResult', queryResult);
+    console.log(`rawQueryResult`, rawQueryResult);
 })()
